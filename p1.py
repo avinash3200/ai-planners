297a608de0b080e90cf8fd22d199c2130fb74ba3
Tushar Agarwal <tushar.agarwal@iitrpr.ac.in>
2016-03-29 15:39:06 +0530

    Add stubs

--- p1.py
+++ p1.py
@@ -3,4 +3,89 @@
 
 # AI Lab 4
 
+class State:
+    """
+    This class represents the current state.
+    """
+
+    def __init__(self):
+        """
+        Initializes a `State` object.
+        """
+
+        # List of TrueSentence objects.
+        # Only has "True" sentences. Rest is assumed False.
+        
+        pass
+
+    def __str__(self):
+        """
+        Returns a human-friendly representation of 
+        the current state.
+        """
+
+        pass
+
+class TrueSentence:
+    """
+    This class represents a sentence whose truth value is "True".
+    """
+
+    def __init__(self):
+        """
+        Initializes a TrueSentence object.
+        """
+
+        # Type of proposition
+        # Argument list
+
+        pass
+
+    def __str__(self):
+        """
+        Returns a human-friendly representation of 
+        the TrueSentence object.
+        """
+
+        pass
+
+
+class Action:
+    """
+    This class represents an action and applies, reverses, and 
+    manages all aspects of actions.
+    """
+
+    def __init__(self):
+        """
+        Initializes an Action object.
+        """
+
+        # Preconditions: a list of TrueSentence objects
+        # Effects: same as above
+
+        pass
+
+    def __str__(self):
+        """
+        Returns a human-friendly representation of 
+        the Action object.
+        """
+
+        pass
+
+    def applyAction(self, stateObject):
+        """
+        Applies an action after unification.
+        """
+
+        pass
+
+    def checkApplicability(self, stateObject):
+        """
+        A boolean function to check if the `self` action can 
+        be applied to the `stateObject` state.
+        """
+
+        pass
 
